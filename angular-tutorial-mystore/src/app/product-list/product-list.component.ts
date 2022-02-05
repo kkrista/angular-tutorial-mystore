3794b992064208213b3030352933f42c78ac51ba
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-02-05 23:54:21 +0200

    code added to file

--- angular-tutorial-mystore/src/app/product-list/product-list.component.ts
+++ angular-tutorial-mystore/src/app/product-list/product-list.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products = products;
 
-  ngOnInit(): void {
+  share() {
+    window.alert('The product has been shared!');
   }
 
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
 }
