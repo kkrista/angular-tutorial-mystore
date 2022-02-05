4a2710d2e8484ece733a320f80e33072f33afb6c
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-02-06 00:04:47 +0200

    products-alerts component created

--- angular-tutorial-mystore/src/app/app.module.ts
+++ angular-tutorial-mystore/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     TopBarComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    ProductAlertsComponent
   ],
   imports: [
     BrowserModule
