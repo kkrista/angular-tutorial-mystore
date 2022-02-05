effab7ac6500fdf319c3811dc4ca5064f03b9685
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-02-05 23:40:23 +0200

    app.component edited

--- angular-tutorial-mystore/src/app/app.module.ts
+++ angular-tutorial-mystore/src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ToBarComponent } from './to-bar/to-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToBarComponent,
     ProductListComponent,
     TopBarComponent
   ],
